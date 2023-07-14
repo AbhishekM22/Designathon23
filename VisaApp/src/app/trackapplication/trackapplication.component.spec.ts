bc699a2ae811593e9d9f47471c09173e59e1fe35
AbhishekM22 <abhishekmoharir6@gmail.com>
2023-07-14 20:56:48 +0530

    Added tracking page

---
+++ VisaApp/src/app/trackapplication/trackapplication.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TrackapplicationComponent } from './trackapplication.component';
+
+describe('TrackapplicationComponent', () => {
+  let component: TrackapplicationComponent;
+  let fixture: ComponentFixture<TrackapplicationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TrackapplicationComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TrackapplicationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
